2a015e93f4887e0715034985efc72ae7acc03a69
Mokhinur Abdurakhimova <86318286+mokhinurabdurakhimova@users.noreply.github.com>
2022-01-22 03:40:32 -1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# import libaries:
 import cv2
 import mediapipe as mp
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
